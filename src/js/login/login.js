5862d65316de74103e9b727d07567a516a9038b8
Rohit Kumar <rohit96682@gmail.com>
2023-01-11 23:13:36 +0530

    login disable btn

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -4,11 +4,29 @@ const userName = qs("#userName");
 const password = qs("#password");
 
 const handleLogin = qs("#handleLogin");
+handleLogin.disabled = true;
+
+userName.addEventListener("change", (e) => {
+  if (password.value === "") {
+    handleLogin.disabled = true;
+  } else {
+    handleLogin.disabled = false;
+  }
+});
+password.addEventListener("change", (e) => {
+  if (password.value === "") {
+    handleLogin.disabled = true;
+  } else {
+    handleLogin.disabled = false;
+  }
+});
+
 handleLogin.addEventListener("click", (e) => {
   e.preventDefault();
 
+  // handleLogin.setAttribute("data-bs-toggle", "modal");
+  // handleLogin.setAttribute("data-bs-target", "#exampleModal");
   const data = new FormData();
-
   data.append("username", userName.value);
   data.append("password", password.value);
 
